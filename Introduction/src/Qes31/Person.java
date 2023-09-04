01344469c740b6de1fa7d5a9fa7c3b607a1f015d
shota-shiba <syota.0802.2@gmail.com>
2023-09-04 18:33:13 +0900

    shota-shibades 9/4 課題1_31 提出

--- Introduction/src/Qes31/Person.java
+++ Introduction/src/Qes31/Person.java
@@ -30,11 +30,15 @@ class Person{
 		System.out.println("「合計"+ this.count +"人です」");
 	}
 	
-	
+	// 問題6：Personクラスにインスタンスメソッド「buy」を定義しましょう。（仮引数：car）
 	public void buy(Car car) {
+		// 問題7：buyメソッドの中でsetOwnerメソッドとthisを用いてownerフィールドの値をセットしましょう。
 		car.setOwner(this.name);
+		// 問題8：さらに、「〇〇が購入しました」と出力する処理を追加してください。
 		System.out.println(car.getOwner() + "が購入しました");
 	}
+	
+	// 問題9：引数の型が異なるbuyメソッドを定義しましょう。（仮引数：bicycle）
 	public void buy(Bicycle bicycle) {
 		bicycle.setOwner(this.name);
 		System.out.println(bicycle.getOwner() + "が購入しました");
